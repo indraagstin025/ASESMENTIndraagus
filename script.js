75b7a0d04726cc8f3a1ad1c11b2dcbd34f20e948
indraagustin025 <indraagustin012@gmail.com>
2023-11-10 14:01:27 +0700

    Asessment Indra

---
+++ script.js
@@ -0,0 +1,10 @@
+function changeHeadingText()
+{
+    var heading = document.getElementById('heading');
+    heading. innerHTML = ' Tetap Semangat  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo, perferendis aut incidunt fugiat vero est ipsam labore pariatur! Ipsam sint consectetur repellendus officiis dolorum voluptate, iste corporis esse vel eligendi laudantium laborum temporibus voluptas minima ratione obcaecati odio magnam repudiandae libero sed natus animi reiciendis unde? Cum fuga optio pariatur, tempore quis corrupti eius?';
+}
+function changeImage()
+{
+    var Img = document.getElementById('img');
+    Img.src = 'Gambar2.jpg';
+}
